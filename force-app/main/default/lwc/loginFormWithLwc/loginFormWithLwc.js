4984bbbfab4b0b17132e63fd40b5deec0a1d4779
Yosuke Kagisaki <kagisaki@gmail.com>
2021-02-03 23:53:36 +0900

    ログインフォーム改修中途保存

--- force-app/main/default/lwc/loginFormWithLwc/loginFormWithLwc.js
+++ force-app/main/default/lwc/loginFormWithLwc/loginFormWithLwc.js
@@ -1,12 +1,23 @@
-import { LightningElement, api, wire } from 'lwc';
-import login from '@salesforce/apex/CommunityLoginWithLWCController.login';
+import { LightningElement, api, wire } from "lwc";
+import login from "@salesforce/apex/CommunityLoginWithLWCController.login";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+
 export default class LoginFormWithLwc extends LightningElement {
-    corporateNumber = '';
+  corporateNumber = "";
 
-    @wire(login, {corporateNumber : '$corporateNumber'})
-    wiredAccount;
+  @wire(login, { corporateNumber: "$corporateNumber" })
+  result;
 
-    handleLogin(event) {
-        
+  handleLoginAction() {
+    console.log("🐣");
+    if (result) {
+      window.location.href = "/";
+    } else {
+      const toast = new ShowToastEvent({
+        title: "ログイン失敗",
+        message: this.error,
+        variant: "error"
+      });
     }
+  }
 }
